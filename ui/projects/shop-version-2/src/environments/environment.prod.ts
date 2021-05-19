3d690e86d9b403646d260ab15d4046a7b6089851
mahesh K S <mahesh25242@gmail.com>
2021-05-19 09:54:58 +0530

    updated still here

--- ui/projects/shop-version-2/src/environments/environment.prod.ts
+++ ui/projects/shop-version-2/src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   production: true,
   siteName: document.querySelector("meta[name=shop-name]").getAttribute("content"),
   shopKey: document.querySelector("meta[name=shop-key]").getAttribute("content"),
+  demoShopKey: '3d9f5a8eec71764c7c2df5a56496c8a1320dd921',
   baseUrl: env.baseUrl,
   lumenSecret: env.lumenSecret,
   client_id: env.client_id,
