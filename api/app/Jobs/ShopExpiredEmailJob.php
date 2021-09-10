c4972825e698adfbd0d48980b7fe66a164c5999d
mahesh K S <mahesh25242@gmail.com>
2021-09-10 08:15:05 +0530

    updated in serever

--- api/app/Jobs/ShopExpiredEmailJob.php
+++ api/app/Jobs/ShopExpiredEmailJob.php
@@ -6,6 +6,8 @@ namespace App\Jobs;
 use App\Mail\ShopExpiredNotification;
 use Mail;
 use App\Models\ShopRenewal;
+use App\Models\Setting;
+use App\Models\Shop;
 
 class ShopExpiredEmailJob extends Job
 {
@@ -30,22 +32,29 @@ class ShopExpiredEmailJob extends Job
      */
     public function handle()
     {
+
+        $settings = Setting::where("name", "shop_expiry_email")->get()->first();
+        $adminEMail = $settings->value;
+
+
         $shopRenewal = ShopRenewal::where("to_date" ,'<', \Carbon\Carbon::now())->whereHas("shop", function($q){
             $q->where('status', 1);
         });
 
         $shopRenewal->chunk(100, function ($shop_renewals)  {
             foreach ($shop_renewals as $shop_renewal) {
-                 $email = new ShopExpiredNotification($shop_renewal->shop);
-                 $toEMail = $shop_renewal->shop->email;
-                 if(env('APP_ENV') == 'local'){
-                    $toEMail = env('DEVELOPER_MAIL');
+                $shop = Shop::find($shop_renewal->shop_id);
+                $email = new ShopExpiredNotification($shop);
+                $toEMail = $shop->email;
+                if(env('APP_ENV') == 'local'){
+                $toEMail = env('DEVELOPER_MAIL');
                     Mail::to($toEMail)->send($email);
-                 }else{
+                }else{
+                    Mail::to($adminEMail)->send($email);
                     Mail::to($toEMail)->send($email);
-                 }
-                 $shop_renewal->shop->status = 0;
-                 $shop_renewal->shop->save();
+                }
+                $shop->status = 0;
+                $shop->save();
             }
           });
 
