d45e9e929520411f8903ec3bb5851393f7ee8d36
mahesh K S <mahesh25242@gmail.com>
2021-09-15 10:06:37 +0530

    c

--- api/resources/views/email/layouts/app.blade.php
+++ api/resources/views/email/layouts/app.blade.php
@@ -1,8 +1,36 @@
-<div>
-    <div>
-        <h3>Online Cart.in</h3>
-    </div>
-    <div>
-        @yield('content')
-    </div>
-</div>
+<table border="0" class="m_4510121335873832133phoenix-email-container" cellspacing="0" cellpadding="0" width="512" bgcolor="#FFFFFF" style="background-color:#ffffff;margin:0 auto;">
+   <tbody>
+      <tr>
+         <td bgcolor="#F6F8FA" style="background-color:#f6f8fa;padding:5px 16px 13px;border-bottom:1px solid #ececec">
+            <table border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%!important;min-width:100%!important">
+               <tbody>
+                  <tr>
+                     <td align="right" valign="middle"><a href="https://onlinecarts.in/" style="font-size: 24px;color:#008cc9;display:inline-block;text-decoration:none" target="_blank">OnlineCarts.in</a></td>
+                     <td width="1">&nbsp;</td>
+                  </tr>
+               </tbody>
+            </table>
+         </td>
+      </tr>
+      <tr>
+         <td>
+            <table border="0" cellspacing="0" cellpadding="0" width="100%">
+               <tbody>
+                  <tr>
+                     <td>
+
+                        <div>
+                            @yield('content')
+                        </div>
+
+                        <h2 style="background-color:#f3f6f8;padding:16px 20px;margin:0;color:#262626;font-weight:200;font-size:20px;line-height:1.2;text-align:center">Thank You</h2>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </td>
+      </tr>
+   </tbody>
+</table>
+
+
