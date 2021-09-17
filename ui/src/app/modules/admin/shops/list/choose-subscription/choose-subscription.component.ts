0e9e23d78040498317a31cca4661f0d7ba54c04c
mahesh K S <mahesh25242@gmail.com>
2021-09-17 17:20:27 +0530

    not updated

--- ui/src/app/modules/admin/shops/list/choose-subscription/choose-subscription.component.ts
+++ ui/src/app/modules/admin/shops/list/choose-subscription/choose-subscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import { Package, Shop } from 'src/app/lib/interfaces';
@@ -10,12 +11,17 @@ import { PackageService } from 'src/app/lib/services';
   styleUrls: ['./choose-subscription.component.scss']
 })
 export class ChooseSubscriptionComponent implements OnInit {
+  package_id = new FormControl('');
   packages$: Observable<Package[]>
   @Input() shop: Shop;
   constructor(private packageService: PackageService,
     public modal: NgbActiveModal) { }
 
+  save(){
+    console.log(this.package_id.value)
+  }
   ngOnInit(): void {
+
     this.packages$ = this.packageService.listAllPackages(1);
   }
 
