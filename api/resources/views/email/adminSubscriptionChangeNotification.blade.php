dd97d5a405f5d5b90f1ae49bd7634b8ee178a72d
mahesh K S <mahesh25242@gmail.com>
2021-09-21 11:51:28 +0530

    updated still here

--- api/resources/views/email/adminSubscriptionChangeNotification.blade.php
+++ api/resources/views/email/adminSubscriptionChangeNotification.blade.php
@@ -3,9 +3,20 @@
     <p>Hello  {{ $shopRenewal->shop->name }},</p>
     @if ( $shopRenewal->amount > 0 ) Your payment {{ $shopRenewal->amount }} is received, Thank you.@endif Your subscription was re-newed and now the current shop expiration date is: {{ $shopRenewal->to_date }}
 
+    @if ( $shopRenewal->comments )
+        Comment from Admin
+        <p>{{ $shopRenewal->comments }}</p>
+    @endif
+
+    @if ( $shopRenewal->attachement )
+        <a href="https://api.onlinecarts.in/assets/shop/{{ $shopRenewal->shop->shop_key }}/general/{{ $shopRenewal->attachement }}">Click here to view submited receipt</a>
+    @endif
+
     @if ( $shopRenewal->amount > 0 )
         Please find attachment for the invoice
     @endif
+
+
 
     If you have any query related to this please use your Subscription id as <b>{{ $shopRenewal->id }}</b>
 @endsection
