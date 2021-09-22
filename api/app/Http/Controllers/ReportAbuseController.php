1b823b1d3c86e8177afccd1ce905148d33b8f8df
mahesh K S <mahesh25242@gmail.com>
2021-09-22 16:55:01 +0530

    updated

--- api/app/Http/Controllers/ReportAbuseController.php
+++ api/app/Http/Controllers/ReportAbuseController.php
@@ -60,7 +60,7 @@ class ReportAbuseController extends Controller
         $perPage = $request->input("perPage", 50);
 
         $perPage = (is_numeric($perPage) && $perPage> 0) ? $perPage : 50;
-        $reportAbuse = \App\Models\ReportAbuse::with(["shopProduct.shopProductCategory", "reportAbuseType"])->paginate($perPage);
+        $reportAbuse = \App\Models\ReportAbuse::with(["shopProduct.shopProductCategory", "reportAbuseType"])->latest()->paginate($perPage);
         return response( $reportAbuse);
     }
 
