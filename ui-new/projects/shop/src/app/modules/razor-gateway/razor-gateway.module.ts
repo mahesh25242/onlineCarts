082c246343694251772c640185d0e216053c4dab
Mahesh K S <mahesh25242@gmail.com>
2021-12-29 11:58:32 +0530

    angular 13

--- ui-new/projects/shop/src/app/modules/razor-gateway/razor-gateway.module.ts
+++ ui-new/projects/shop/src/app/modules/razor-gateway/razor-gateway.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RazorGatewayComponent } from './razor-gateway/razor-gateway.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [  
     RazorGatewayComponent
   ],
   imports: [
-    CommonModule,                     
+    CommonModule,     
+    MatButtonModule                
   ],
   exports:[    
     RazorGatewayComponent
